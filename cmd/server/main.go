77d259d04be32e1d458530fe93a5c8bcc3464d7b
Rodrigo Fuenzalida <rfuenzalidac87@gmail.com>
2019-01-17 09:46:58 -0300

    change db name

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -18,7 +18,7 @@ import (
 
 func main() {
 	port := flag.Int64("port", 8001, "listening port")
-	postgresDSN := flag.String("postgres-dsn", "postgres://localhost:5432/syracuse?sslmode=disable", "Postgres DSN")
+	postgresDSN := flag.String("postgres-dsn", "postgres://localhost:5432/drachma?sslmode=disable", "Postgres DSN")
 
 	flag.Parse()
 	fmt.Println(*postgresDSN)
