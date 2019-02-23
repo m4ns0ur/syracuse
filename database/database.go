9e1c6f3a961ccb63cdbd2dab893fc5f50b5cfdc2
Rodrigo Fuenzalida <rfuenzalidac87@gmail.com>
2019-02-23 17:22:11 -0300

    database: Expose an interface to define database services

---
+++ database/database.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"github.com/go-toschool/syracuse"
+	"github.com/go-toschool/syracuse/database/postgres"
+	"github.com/jmoiron/sqlx"
+
+	_ "github.com/lib/pq"
+)
+
+// Store define the behavior for a Store.
+type Store interface {
+	Get(*syracuse.CitizensQuery) (*syracuse.Citizen, error)
+	Select() ([]*syracuse.Citizen, error)
+	Create(*syracuse.Citizen) error
+	Update(*syracuse.Citizen) error
+	Delete(*syracuse.Citizen) error
+}
+
+// NewPostgres return a new citizens service.
+func NewPostgres(dsn string) (Store, error) {
+	db, err := sqlx.Connect("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	return &postgres.CitizensStore{
+		Store: db,
+	}, nil
+}
